63022e3d848ae731ae53e69895fea4595b9871f6
Quadri <ariyoquadri4@gmail.com>
2026-04-03 12:54:48 +0100

    Initial commit

--- Doughnut2.java
+++
@@ -1,5 +0,0 @@
-public class Doughnut2{
-  public static void main(String[] args){
-        System.out.println("Quadri\nAriyo\n2026");
-    }
-} 
--- Doughnut4.java
+++
@@ -1,7 +0,0 @@
-public class Doughnut4{
-  public static void main(String[] args){
-
-    String name = "Ryan";
-        System.out.println("Hello, "+ name);
-    }
-} 
