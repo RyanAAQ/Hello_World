8b97d0e1d22f24174ff516a47de306e6c0c69bf6
Quadri <ariyoquadri4@gmail.com>
2026-04-05 15:39:56 +0100

    Added new files

--- EasterEggs/Meatpie/TaskSeven.java
+++ EasterEggs/Meatpie/TaskSeven.java
@@ -5,9 +5,9 @@ public class TaskSeven{
         Scanner input = new Scanner(System.in);
 
     System.out.println("Enter first number");
-    int num = input.nextInt();
+    int num1 = input.nextInt();
 
-    int square = num * num;
+    int square = num1 * num1;
 
     System.out.printf("The square is %d%n", square);
     }
