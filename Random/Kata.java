a659c205cf517fe71527ec0c41ffb4b4d8c84c1f
Ryan <ariyoquadri4@gmail.com>
2026-04-20 08:19:49 +0100

    Added new files

--- Random/Kata.java
+++ Random/Kata.java
@@ -1,6 +1,6 @@
 public class Kata {
     public static void main(String[] args) {
-        // To call these, we provide the types and use the method names
+
         maximum(419, 234);
         isEven(234);
         subtract(333, 999);
@@ -8,24 +8,22 @@ public class Kata {
     }
 
     public static int maximum(int firstNumber, int secondNumber) {
-        int maxNumber = firstNumber; // Removed extra 'int'
+        int maxNumber = firstNumber;
         if (secondNumber > maxNumber) {
             maxNumber = secondNumber;
         }
         System.out.printf("The Maximum number = %d%n", maxNumber);
         return maxNumber;
     }
-
     public static boolean isEven(int number) {
         if (number % 2 == 0) {
             System.out.printf("The integer %d is Even%n", number);
-            return true; // Must return a boolean (true/false)
+            return true; 
         } else {
             System.out.printf("The integer %d is Odd%n", number);
             return false;
         }
     }
-
     public static int subtract(int firstNumber, int secondNumber) {
         int minus = Math.abs(firstNumber - secondNumber);
         System.out.printf("The difference = %d%n", minus);
@@ -37,7 +35,6 @@ public class Kata {
             System.out.println("0");
             return 0;
         }
-        // Cast to float so we get decimals (e.g., 5/2 = 2.5, not 2)
         float division = (float) firstNumber / secondNumber;
         System.out.printf("The quotient = %.3f%n", division);
         return division;
